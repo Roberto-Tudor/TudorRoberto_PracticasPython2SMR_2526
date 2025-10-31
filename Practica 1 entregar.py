0d315f20e1a92a9c7d8ef0f4a411d20bb2b9709d
Roberto-Tudor <robertotudor27@gmail.com>
2025-10-31 15:05:53 +0100

    Add files via upload

---
+++ Practica 1 entregar.py
@@ -0,0 +1,24 @@
+# import random
+
+# user_pass = [["ana", "abc123"], ["iker", "123abc"], ["ramon", "123"], ["cris", "abc"]]
+# passs = ["1234", "password", "qwerty", "abc123", "hola"]
+
+# while True:
+#     nombre = input("Introduce el nombre del usuario: ")
+#     contraseña = input("Introduce la contraseña del usuario: ")
+#     user_pass.append([nombre, contraseña])
+#     seguir = input("¿Quieres añadir un nuevo usuario? (si/no): ").lower()
+#     if seguir != "si":
+#         break
+
+# usuarios_vulnerados = []
+# usuarios_novulnerado = []
+
+# romper = True
+
+# while romper != passs:
+    
+x = 1
+while x < 4:
+    x += 1
+print(x)
