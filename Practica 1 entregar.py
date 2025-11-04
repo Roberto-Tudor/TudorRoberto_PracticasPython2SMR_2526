f938d348638b88509a0071cbcb4ec218055023df
Roberto-Tudor <robertotudor27@gmail.com>
2025-11-04 18:19:10 +0100

    Add files via upload

--- Practica 1 entregar.py
+++ Practica 1 entregar.py
@@ -1,24 +1,53 @@
-# import random
+import random
 
-# user_pass = [["ana", "abc123"], ["iker", "123abc"], ["ramon", "123"], ["cris", "abc"]]
-# passs = ["1234", "password", "qwerty", "abc123", "hola"]
+usuarios = []
 
-# while True:
-#     nombre = input("Introduce el nombre del usuario: ")
-#     contraseña = input("Introduce la contraseña del usuario: ")
-#     user_pass.append([nombre, contraseña])
-#     seguir = input("¿Quieres añadir un nuevo usuario? (si/no): ").lower()
-#     if seguir != "si":
-#         break
+while True:
+    nombre = input("Introduce el nombre del usuario: ")
+    contraseña = input("Introduce la contraseña del usuario: ")
+    usuarios.append([nombre, contraseña])
+    seguir = input("¿Quieres añadir un nuevo usuario? (si/no): ").lower()
+    if seguir != "si":
+        break
 
-# usuarios_vulnerados = []
-# usuarios_novulnerado = []
 
-# romper = True
+contraseñas_inseguras = ["1234", "password", "qwerty", "abc123", "hola"]
 
-# while romper != passs:
+
+usuarios_vulnerados = [] 
+usuarios_resistentes = [] 
+
+
+seguir_atacando = True
+
+while seguir_atacando:
     
-x = 1
-while x < 4:
-    x += 1
-print(x)
+    usuario = random.choice(usuarios)
+    nombre_usuario = usuario[0]
+    contraseña_usuario = usuario[1]
+
+    
+    for insegura in contraseñas_inseguras:
+        if contraseña_usuario.lower() == insegura.lower():
+            print(f"El usuario {nombre_usuario} fue vulnerado. Contraseña débil.")
+            usuarios_vulnerados.append(nombre_usuario)
+            break
+    else:
+        
+        print(f"El usuario {nombre_usuario} resistió el ataque.")
+        usuarios_resistentes.append(nombre_usuario)
+
+    
+    respuesta = input("¿Quieres hacer otro intento de ataque? (si/no): ").lower()
+    if respuesta != "si":
+        seguir_atacando = False
+
+
+print("Resumen de vulnerabilidades")
+print("Usuarios vulnerados:")
+for u in usuarios_vulnerados:
+    print(f"{u}")
+
+print("Usuarios que resistieron el ataque:")
+for u in usuarios_resistentes:
+    print(f"{u}")
